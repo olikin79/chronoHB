1f0121b46707e6ba59cb11553bb25159544cfb16
olikin79 <lax.olivier@gmail.com>
2023-01-06 14:51:07 +0100

    Create maj.py

---
+++ maj/maj.py
@@ -0,0 +1,10 @@
+## script de mise à jour de chronoHB
+
+def majScript() :
+    # téléchargement du fichier indiquant les fichiers à télécharger depuis la version en cours.
+    # téléchargement 1 par 1 des fichiers mis à jour.
+    # tag de la nouvelle version en cours si succès de tous les fichiers uniquement.
+    # message pour l'utilisateur du succès.
+    showinfo("ATTENTION","Mise à jour vers X.Y réussie.")
+    
+
