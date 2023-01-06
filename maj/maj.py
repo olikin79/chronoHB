3eb863b9ff6f410b9b18d01266925fd74bda757d
olikin79 <lax.olivier@gmail.com>
2023-01-06 22:37:31 +0100

    script de mise àjour standard vide

--- maj/maj.py
+++ maj/maj.py
@@ -4,7 +4,10 @@ def majScript() :
     # téléchargement du fichier indiquant les fichiers à télécharger depuis la version en cours.
     # téléchargement 1 par 1 des fichiers mis à jour.
     # tag de la nouvelle version en cours si succès de tous les fichiers uniquement.
+    # si nécessaire, reboot à programme
+    reboot = False
     # message pour l'utilisateur du succès.
-    showinfo("ATTENTION","Mise à jour vers X.Y réussie.")
+    message = "MISE A JOUR","Le logiciel est à jour en version " + version
+    return reboot, message
     
 
