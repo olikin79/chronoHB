7cb35ea14957bdc72edc060ba76ce6c051170e1c
olikin79 <lax.olivier@gmail.com>
2023-10-02 20:49:09 +0200

    Update resultatsDiffusion.py

--- resultatsDiffusion.py
+++ resultatsDiffusion.py
@@ -13,7 +13,7 @@ from FonctionsMetiers import *
 ##)
 
 
-def replaceDansDiplomeEnFonctionDesResultats(modele, coureur) :
+def replaceDansDiplomeEnFonctionDesResultats(modele, coureur, nomModele) :
     """ remplace les champs du modèle par les informations du coureur fourni"""
     groupement = groupementAPartirDeSonNom(coureur.course,nomStandard = True)
     categorie = "Catégorie " + coureur.categorieSansSexe()
@@ -30,21 +30,21 @@ def replaceDansDiplomeEnFonctionDesResultats(modele, coureur) :
     nbreTotal = str(groupement.nombreDeCoureursTotal)
     nbreTotalCategorie = str(groupement.getTotalParCategorie(coureur.categorieSansSexe(),coureur.sexe))
     rangSexe = formateRangSexe(coureur.rangSexe,coureur.sexe)
-    fondDiplome = diplomeModele + ".jpg" ### pour l'instant le fond utilisé a le même nom que le dossard utilisé.
+    fondDiplome = nomModele + ".jpg" ### pour l'instant le fond utilisé a le même nom que le dossard utilisé.
     # astuce pour éviter que des rangs par catégorie inutiles apparaissent, on change en SENIOR puisque le classement par catégorie
     # pour les séniors revient au même que la classement global.
     cat = coureur.categorieSansSexe()
 ##    if nbreTotalCategorie == "1" :
 ##        # s'il n'y a qu'une seule personne dans une catégorie, est ce que l'on supprime l'affichage ou non en mettant SE artificiellement ?
 ##        cat = "SE"
-    modele = modele.replace("@nom@",coureur.nom).replace("@prenom@",coureur.prenom).replace("@date@",dateDuTrail)\
+    retour = modele.replace("@nom@",coureur.nom).replace("@prenom@",coureur.prenom).replace("@date@",dateDuTrail)\
                 .replace("@intituleCross@",Parametres["intituleCross"]).replace("@lieu@",Parametres["lieu"])\
                 .replace("@rang@",formateRangSexe(coureur.rang, coureur.sexe))\
                 .replace("@nbreTotal@",nbreTotal).replace("@categorie@",categorie).replace("@cat@",cat)\
                 .replace("@logoSexe@",logoSexe).replace("@nomCourse@",nomCourse).replace("@rangCat@",formateRangSexe(coureur.rangCat, coureur.sexe))\
                 .replace("@nbreTotalCategorie@",str(nbreTotalCategorie)).replace("@temps@",temps).replace("@vitesse@",coureur.vitesseFormatee())\
                 .replace("@rangSexe@",rangSexe).replace("@nbreTotalSexe@",str(nbreTotalSexe)).replace("@fondDiplome@",fondDiplome)
-    return modele
+    return retour
 
 def formateRangSexe(rang, sexe) :
     if int(rang) == 1 :
@@ -56,15 +56,18 @@ def formateRangSexe(rang, sexe) :
         retour = str(rang) + "ème"
     return retour
 
-def genereDiplome(modele, coureur) :
-    """ générer un diplome dans un fichier pdf puis le convertit en png"""
+def genereDiplome(modele, coureur, nomModele) :
+    """ générer un diplome dans un fichier pdf puis le convertit en png
+    modele est le texte en tex du diplome
+    coureur est un objet coureur
+    nomModele est le nom du modèle de diplome à utiliser"""
     print("Utilisation de genereDiplome pour le coureur", coureur.nom, coureur.prenom, coureur.dossard)
     TEXDIR = "resultats"+os.sep+"tex"+os.sep
     creerDir(TEXDIR)
     osCWD = os.getcwd()
     file = coureur.dossard
     with open(TEXDIR+file+ ".tex", 'w',encoding="utf-8") as f :
-        f.write(replaceDansDiplomeEnFonctionDesResultats(modele, coureur))
+        f.write(replaceDansDiplomeEnFonctionDesResultats(modele, coureur, nomModele))
     f.close()
     compilateurComplete = compilateur.replace("@dossier@","resultats")
     compilerDossards(compilateurComplete, ".", file + ".tex" , 1)
@@ -75,7 +78,7 @@ def genereDiplome(modele, coureur) :
     if os.path.exists(fichier) :
         cmd = 'start "" /I /wait /min /D . .\\IM\\convert -density 100 ' + fichierAConvertir + " " + fichierDestination
         # options essayées pour une luminosité meilleure : -auto-gamma -white-balance -normalize -auto-level -equalize
-        #print("Exécution de", cmd)
+        print("Exécution de", cmd)
         syscmd(cmd)
         for ext in ["aux", "log", "synctex.gz" ]:
             if os.path.exists("resultats/" + coureur.dossard + "." + ext) :
@@ -83,9 +86,15 @@ def genereDiplome(modele, coureur) :
     else :
         print("Le fichier",fichier,"n'a pas été généré")
 
-def envoiDiplomePourTousLesCoureurs() :
-    # charger le modèle de diplome
-    modeleDiplome = "./modeles/diplomes/" + diplomeModele + ".tex"
+def envoiDiplomePourTousLesCoureurs(diplomeImpose = "") :
+    ''' diffuse les diplomes non encore envoyés aux coureurs '''
+    # pour les tests
+    if diplomeImpose != "" :
+        nomModele = diplomeImpose
+    else :
+        nomModele = Parametres["diplomeModele"]
+        # charger le modèle de diplome des paramètres
+    modeleDiplome = "./modeles/diplomes/" + nomModele + ".tex"
     #pour les tests : modeleDiplome = "./modeles/diplomes/Randon-Trail.tex"
     with open(modeleDiplome , 'r') as f :
         modele = f.read()
@@ -106,16 +115,17 @@ def envoiDiplomePourTousLesCoureurs() :
         if c.temps > 0 and (not c.emailEnvoiEffectue) and c.email and c.nombreDeSecondesDepuisDerniereModif() > 60*diplomeDiffusionApresNMin :
             # le coureur a passé la ligne a un email valide et n'a pas reçu son diplome et n'a pas été modifié récemment, on génère son diplome et on l'envoie
             #print("Envoi du mail fictif pour le coureur",c.nom,c.dossard,c.temps)
-            genereDiplome(modele, c)
+            genereDiplome(modele, c, nomModele)
             if envoiDiplomeParMail(c) :
                 c.setEmailEnvoiEffectue(True)
+        ### pour les tests !
         elif __name__ == '__main__' and c.dossard == "1A" :
-            genereDiplome(modele, c)
+            genereDiplome(modele, c, nomModele)
         elif c.dossard[:-1] != "C" and c.temps == 0.0 :
             print("Condition fausse : ", c.dossard, c.nom, "=>", c.temps, " > 0 and (not ",c.emailEnvoiEffectue,") and", c.email, "and" , c.nombreDeSecondesDepuisDerniereModif()," > 60*",diplomeDiffusionApresNMin)
         #else : #if c.dossard == "1A" :
-         #   print("Dossard", c.dossard ,"non envoyé", c.temps, " > 0 and (not ", c.emailEnvoiEffectue, ") and", c.email ,"and", c.nombreDeSecondesDepuisDerniereModif() ,"> 60*diplomeDiffusionApresNMin")
-            
+        #   print("Dossard", c.dossard ,"non envoyé", c.temps, " > 0 and (not ", c.emailEnvoiEffectue, ") and", c.email ,"and", c.nombreDeSecondesDepuisDerniereModif() ,"> 60*diplomeDiffusionApresNMin")
+        
             
 def envoiDiplomeParMail(coureur):
     fichier = "resultats/" + coureur.dossard + ".png"
@@ -152,6 +162,6 @@ def envoiDiplomeParMail(coureur):
         print("Erreur inconnue générée lors de l'envoi de l'email avec le diplome.")
 
 if __name__ == '__main__':
-    diplomeModele = "Randon-Trail"
-    envoiDiplomePourTousLesCoureurs()
+    diplomeImpose = "cross-HB"
+    envoiDiplomePourTousLesCoureurs(diplomeImpose=diplomeImpose)
         
