dcd515fb9a7ee96ca2e4a0d209262ebde6740d0f
olikin79 <lax.olivier@gmail.com>
2024-11-18 22:10:57 +0100

    Update setup_windows.py

--- setup_windows.py
+++ setup_windows.py
@@ -8,7 +8,7 @@ build_exe_options = {
     "includes": ["tkinter", "time", "datetime", "webbrowser", "subprocess", "sys", "os", "re", "copy", "socket",\
         "idlelib.tooltip", "pprint", "cgi", "requests", "importlib", "hashlib", "CameraMotionDetection", "functools", \
         "cryptography.fernet", "ftplib", "locale", "glob", "shutil", "random", "csv", "pathlib", "server", "threading", \
-        "xlswriter", "math", "openpyxl", "urllib.request", "zipfile", "redmail", "cryptography.fernet"],
+        "xlsxwriter", "math", "openpyxl", "urllib.request", "zipfile", "redmail", "cryptography.fernet"],
     "include_files": ["favicon.ico","create_shortcuts.py", \
         "CameraMotionDetection.py", "FonctionsAssistance.py", "FonctionsDiffusionInternet.py", "FonctionsMetiers.py", "jquery-3.6.0.js", \
         "LICENSE", "mystyle.css", "mystyle_mode-sombre.css", "mystyleWeb.css", "openh264-1.8.0-win64.dll", "resultatsDiffusion.py", \
@@ -22,10 +22,25 @@ build_exe_options = {
 build_msi_options = {"upgrade_code": "{66212526-7761-6E69-6F6E-6C129E797817}",
                      "target_name" :"ChronoHB",
                      "data": {
-                        "Shortcut": [
-    ("DesktopShortcut", "DesktopFolder", "ChronoHB", "TARGETDIR", "[TARGETDIR]ChronoHB.exe", None, "favicon.ico", None, None, None, None, 'TARGETDIR'),
-    ("StartMenuShortcut", "ProgramMenuFolder", "ChronoHB", "TARGETDIR", "[TARGETDIR]ChronoHB.exe", None, "favicon.ico", None, None, None, None, 'TARGETDIR')
-                                ]
+                            "Shortcut": [
+        ("DesktopShortcut", "DesktopFolder", "ChronoHB", "TARGETDIR", "[TARGETDIR]ChronoHB.exe", None, "favicon.ico", None, None, None, None, 'TARGETDIR'),
+        ("StartMenuShortcut", "ProgramMenuFolder", "ChronoHB", "TARGETDIR", "[TARGETDIR]ChronoHB.exe", None, "favicon.ico", None, None, None, None, 'TARGETDIR')
+                                    ],
+                        # Associer l'extension .chb au type ChronoHBFile
+                        "Extension": [
+                            (".chb", "TARGETDIR", "ChronoHBFile", 0, 1)
+                        ],
+                        # Associer le type ChronoHBFile à l'exécutable chronoHB.exe
+                        "FileType": [
+                            ("ChronoHBFile", "ChronoHB File", "[TARGETDIR]chronoHB.exe", None, None)
+                        ]
+                        # "Extension": [
+                        #         # Association pour les fichiers .chb
+                        #         ("Extension", "FileType", ".chb", "ChronoHBFile")
+                        #         ],
+                        # "FileType": [
+                        #         ("FileType", None, "ChronoHB File", "[TARGETDIR]chronoHB.exe", None)
+                        #         ]
                             }
                     }
 
